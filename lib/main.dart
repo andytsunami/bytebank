20b50610b5f993209a70588392a688117fe2a2c0
André Vasconcelos <andre.bruno@resourceit.com>
2019-12-05 15:33:14 -0300

    Trocando retorno de lista para Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,27 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-        home: Scaffold(
-      body: ListaTransferencia(),
-      appBar: AppBar(
-        title: Text("Transferencias"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
-    )));
+        home: ListaTransferencia()));
 
 class ListaTransferencia extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(children: <Widget>[
-      ItemTransferencia(Transferencia(1.000, 1000)),
-      ItemTransferencia(Transferencia(2.000, 1000)),
-      ItemTransferencia(Transferencia(3.000, 1000)),
-      ItemTransferencia(Transferencia(4.000, 1000)),
-      ItemTransferencia(Transferencia(5.000, 1000)),
-      ItemTransferencia(Transferencia(6.000, 1000)),
-      ItemTransferencia(Transferencia(7.000, 1000)),
-    ]);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Transferencias"),
+      ),
+      body: Column(children: <Widget>[
+        ItemTransferencia(Transferencia(1.000, 1000)),
+        ItemTransferencia(Transferencia(2.000, 1000)),
+        ItemTransferencia(Transferencia(3.000, 1000)),
+        ItemTransferencia(Transferencia(4.000, 1000)),
+
+      ]),
+
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
 
